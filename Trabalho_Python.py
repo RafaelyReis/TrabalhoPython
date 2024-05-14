e3f123fd5b42d5c463b1367fe9d361acb4cc6bd5
Beatriz Salles <beatriz-lua123@hotmail.com>
2024-05-14 15:38:27 -0300

    Versão final do trabalho em python
    Usamos métodos com if, elif e else para os caminhos selecionados do usuário.

--- Trabalho_Python.py
+++ Trabalho_Python.py
@@ -1,4 +1,16 @@
+import sys
+
+
 def calcular_tmb(peso, altura, idade, sexo):
+    
+    #Calcula a Taxa de Metabolismo Basal (TMB) usando a equação de Harris-Benedict.
+    
+    #peso (float): Peso do usuário em kg.
+    #altura (float): Altura do usuário em cm.
+    #idade (int): Idade do usuário em anos.
+    #sexo (str): Sexo do usuário ("masculino" ou "feminino").
+ 
+    #Return:(float): TMB calculada.
     
     if sexo == "masculino":
      tmb = 66 + (13.7 * peso) + (5 * altura) - (6.8 * idade)
@@ -14,7 +26,12 @@ def calcular_tmb(peso, altura, idade, sexo):
     return tmb
 
 def calcular_macronutrientes(peso):
-   
+    
+    #Calcula a quantidade de macronutrientes com base no peso do usuário.
+    
+    #peso (float): Peso do usuário em kg.    
+    #Return: Quantidade de proteína, carboidratos e gorduras em gramas.
+    
     prot = 2 * peso 
     carb = 3 * peso
     gord = 0.5 * peso
@@ -22,16 +39,26 @@ def calcular_macronutrientes(peso):
     return prot, carb, gord
 
 def calcular_gasto_calorico(exercicio, peso, tempo):
-    tabela_met = {"musculacao_media": 3.5, "musculacao_alta": 6}
+    
+
+    #musculação media
+    if exercicio == 1:
+        return 3.5 * peso * tempo
+    
+    #musculação alta
+    elif exercicio == 2:
+         return 6 * peso * tempo
+  
+    #sair
+    elif exercicio == 3:
+        print("Saindo do programa!")
+        sys.exit()
    
-    if exercicio in tabela_met:
-        met = tabela_met[exercicio] * peso * tempo
-   
+   #nao selecionou nenhuma das opções
     else:
-        met = None
         print("Exercício não reconhecido.")
-   
-    return met
+        return None
+ 
 
 def main():
     peso = float(input("Digite seu peso (em kg): "))
@@ -47,13 +74,22 @@ def main():
         prot, carb, gord = calcular_macronutrientes(peso)
         print(f"Para atender suas necessidades diárias, consuma {prot:.2f}g de proteína, {carb:.2f}g de carboidratos e {gord:.2f}g de gordura.")
 
-        exercicio = input("Digite o exercício que você pratica: ")
+        #Pergunta que faremos a pessoa.
+        print("Digite o número do exercício que você pratica:")
+        print("1 - Musculção média")
+        print("2 - Musculção alta")
+        print("3 - Não pratico nenhum desses exercícios / Sair")
+        exercicio = int(input("Digite o numero do exercício desejado: "))
         tempo = float(input("Por quanto tempo você pratica essa atividade em horas: "))
         gasto_calorico = calcular_gasto_calorico(exercicio, peso, tempo)
        
         if gasto_calorico is not None:
-            print(f"Durante {tempo} horas de {exercicio}, você queimou {gasto_calorico:.2f} calorias.")
-   
+            if exercicio == 1:
+                print(f"Durante {tempo} horas de Musculação média, você queimou {gasto_calorico:.2f} calorias.")
+            elif exercicio == 2:
+                print(f"Durante {tempo} horas de Musculação alta, você queimou {gasto_calorico:.2f} calorias.")
+        else:
+            print("Exercício não reconhecido.")
     else:
         print("Não foi possível calcular a TMB. Verifique os dados fornecidos.")
 
